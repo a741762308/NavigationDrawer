423f81d22c4268790db585157af7d430a63993b2
dq <dongqing123@126.com>
2016-05-25 13:38:05 +0800

    CollapsingToolbarLayout

--- app/src/main/java/com/jsqix/dq/navigation/BlankActivity.java
+++ app/src/main/java/com/jsqix/dq/navigation/BlankActivity.java
@@ -1,39 +1,55 @@
 package com.jsqix.dq.navigation;
 
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.jsqix.dq.navigation.view.RefreshLayout;
+import com.jsqix.dq.navigation.adapter.RecyclerViewAdapter;
 
 public class BlankActivity extends AppCompatActivity {
-    private RefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_redresh);
-        refreshLayout = (RefreshLayout) findViewById(R.id.refresh);
-        refreshLayout.setOnPullLoadMoreListener(new RefreshLayout.PullLoadMoreListener() {
-            @Override
-            public void onRefresh() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        refreshLayout.setPullLoadMoreCompleted();
-                    }
-                }, 1000);
-            }
+        setContentView(R.layout.activity_blank);
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("哆啦A梦");
+        RecyclerView mRecyclerView= (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(new RecyclerViewAdapter(this));
+    }
 
-            @Override
-            public void onLoadMore() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        refreshLayout.setPullLoadMoreCompleted();
-                    }
-                }, 1000);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.set, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            if (id == R.id.action_settings) {
+                return true;
             }
-        });
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jsqix/dq/navigation/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/jsqix/dq/navigation/adapter/RecyclerViewAdapter.java
@@ -1,17 +1,19 @@
 package com.jsqix.dq.navigation.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jsqix.dq.navigation.BlankActivity;
 import com.jsqix.dq.navigation.R;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private Context mContext;
-    private int mCount = 2;
+    private int mCount = 10;
 
     public RecyclerViewAdapter(Context mContext) {
         this.mContext = mContext;
@@ -26,7 +28,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(final RecyclerViewAdapter.ViewHolder holder, int position) {
-        final View view = holder.mView;
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(mContext, BlankActivity.class);
+                mContext.startActivity(mIntent);
+            }
+        });
 
     }
 
